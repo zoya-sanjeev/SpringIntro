44c15c03df21da5a8b0d5a55c21cc7a6d2eeb09b
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-28 17:42:25 +0530

    ADDED: initialized project structure

---
+++ src/test/java/com/bridgelabz/helloworlddemo/HelloWorldDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.helloworlddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
