6bc35372d773449bc1e51d2e1a24ecc78897242d
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-28 17:49:02 +0530

    ADDED: HelloWorldDemo app made to display hello world message

--- src/main/java/com/bridgelabz/helloworlddemo/HelloWorldDemoApplication.java
+++ src/main/java/com/bridgelabz/helloworlddemo/HelloWorldDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello From Bridgelabz");
 		SpringApplication.run(HelloWorldDemoApplication.class, args);
 	}
 
