e8e04e39a28a6ebc36ee43ff185ad69bbb3fe93d
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-28 18:08:11 +0530

    ADDED: mapping to hello.html page added to controller

--- src/main/java/com/bridgelabz/helloworlddemo/controller/HelloWebController.java
+++ src/main/java/com/bridgelabz/helloworlddemo/controller/HelloWebController.java
@@ -1,9 +1,13 @@
 package com.bridgelabz.helloworlddemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloWebController {
-	
+	@GetMapping("/web")
+	public String hello() {
+		return "hello";
+	}
 
 }
