79376dff71ff9bb8bb7bc79d27001d7c36c9ed3c
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-28 18:16:28 +0530

    ADDED: mapping for message.html added

--- src/main/java/com/bridgelabz/helloworlddemo/controller/HelloWebController.java
+++ src/main/java/com/bridgelabz/helloworlddemo/controller/HelloWebController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.helloworlddemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,6 +9,12 @@ public class HelloWebController {
 	@GetMapping("/web")
 	public String hello() {
 		return "hello";
+	}
+	
+	@GetMapping("/web/message")
+	public String message(Model model) {
+		model.addAttribute("message", "This is a custom message");
+		return "message";
 	}
 
 }
